7fc11ae98e24928e22762677736bea02e8e9216b
TrangVu <trangvuuet@gmail.com>
2016-07-06 09:10:32 +0700

    remove unneccessary file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: "John", email:"ttnhdvn@gmail.com", password:"Trang1912", password_confirmation:"Trang1912")
