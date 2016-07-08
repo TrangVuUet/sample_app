e08a3eb1e9533e9b54d072d1a29412268bc77722
TrangVu <trangvuuet@gmail.com>
2016-07-08 13:35:59 +0700

    fixed bugs in advanced-login

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20160707061832) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "remember_digest"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "password_digest"
